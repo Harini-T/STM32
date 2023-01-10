851b1e66c48856950ee6c3c65a2c0bedf153c900
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-01-10 13:54:22 +0530

    002 Operating modes

--- armcortex-mx/cortexmx/001HelloWorld/Src/main.c
+++ armcortex-mx/cortexmx/001HelloWorld/Src/main.c
@@ -22,8 +22,10 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#include<stdio.h>
 int main(void)
 {
+	printf("Hello World");
     /* Loop forever */
 	for(;;);
 }
