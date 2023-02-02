03a14127999bb0d1652ab1344f723b9046fbfabf
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-02-02 11:58:08 +0530

    bit_banding

--- armcortex-mx/007_bit_banding/Src/main.c
+++ armcortex-mx/007_bit_banding/Src/main.c
@@ -17,13 +17,30 @@
  */
 
 #include <stdint.h>
-
+#include <stdio.h>
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+
+#define ALIAS_BASE 0x22000000U
+#define BITBAND_BASE 0x20000000U
+
 int main(void)
 {
+	uint8_t *ptr = (uint8_t*)0x20000200;
+	*ptr = 0xff;
+	//Normal method
+	//Clearing the 7th bit position
+	*ptr &= ~(1<<7);
+	//reset to 0xff
+	*ptr = 0xff;
+
+	//bit band method
+	uint8_t *alias_addr = (uint8_t *)(ALIAS_BASE + (32 *(0x20000200 - BITBAND_BASE)) + 7*4);
+
+	//clearing 7th bit of address 0x20000200
+	*alias_addr = 0;
     /* Loop forever */
 	for(;;);
 }
