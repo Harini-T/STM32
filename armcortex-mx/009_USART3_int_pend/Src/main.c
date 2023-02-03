62363fc0b8cf706cd5c55501e9737973b42244a3
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-02-03 09:59:37 +0530

    usart3_int_pend

--- armcortex-mx/009_USART3_int_pend/Src/main.c
+++ armcortex-mx/009_USART3_int_pend/Src/main.c
@@ -17,13 +17,28 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#define USART3_IRQNO	39
+
 int main(void)
 {
+	//Manually pend the pending bit for USART3 IRQ number in NVIC
+	uint32_t *pISPR1 = (uint32_t*)0xE000E204;
+	*pISPR1 |= (1<<(USART3_IRQNO % 32));
+
+	//Enable the USART3 IRQ number in NVIC
+	uint32_t *pISER1 = (uint32_t*)0xE000E104;
+	*pISER1 |= (1<<(USART3_IRQNO % 32));
+
     /* Loop forever */
 	for(;;);
+}
+
+void USART3_IRQHandler(void){
+	printf("in USART3 isr\n");
 }
